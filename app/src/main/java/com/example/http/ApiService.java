b2b284e3a68d20a172ddbcf64815b6f08d8a6255
leeseonseonje <leeseonje9323@gmail.com>
2021-09-01 20:59:30 +0900

    2021-09-01

--- app/src/main/java/com/example/http/ApiService.java
+++ app/src/main/java/com/example/http/ApiService.java
@@ -33,6 +33,7 @@ import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
@@ -95,7 +96,7 @@ public interface ApiService {
     @GET("/notice/{noticeId}")
     Call<ResponseNoticeDto> noticeContent(@Header("Authorization") String accessToken, @Path("noticeId") Long noticeId);
 
-    @POST("/notice")
+    @PATCH("/notice")
     Call<Void> noticeUpdate(@Header("Authorization") String accessToken, @Body RequestNoticeUpdateDto requestNoticeUpdateDto);
 
     @DELETE("/notice/{noticeId}")
